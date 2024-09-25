a99ebf73e73545a0803c400c518e38e61f027002
nessco1234 <app1.nessco@gmail.com>
2024-09-25 16:49:44 +0530

    everything working fine

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="google-site-verification" content="ikClytgU_EOmadP85IyHWHzVI5JDsCEE2MX2zqXYVPs" />
+        <meta name="robots" content="index,follow" />
       </Head>
       <Component {...pageProps} />
     </>
